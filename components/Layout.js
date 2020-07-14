53887233c70d7bd9717efebd68aa76548ce26088
taajuddin <taajraza@gmail.com>
2020-07-14 14:48:24 +0530

    first commit

---
+++ components/Layout.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Navbar from '../components/Navbar'
+
+const Layout=(props)=>(
+    <div>
+        <Head>
+            <title>BitzPrices</title>
+            <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css" />
+        </Head>
+        <Navbar/>
+        {props.children}
+    </div>
+)
+export default Layout
---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout'
+const About = ()=>(
+<Layout>
+<div>  
+    <h1>About BitzPrices</h1>
+    <p>Application to view bitcoins Prices</p>
+</div>
+</Layout>
+)
+export default About
